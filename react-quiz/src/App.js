0cde48084a933f78dcab6ccff937679fb0699b2e
Vasudevshetty <vasudeepu2815@gmail.com>
2024-05-03 23:42:16 +0530

    Initalised the project well, should add functinaliyt, learnt how to setup a fake api from json-server npm package.

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,7 +1,44 @@
-import "./App.css";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataFetched":
+      return { ...state, questions: action.payload };
+    default:
+      throw new Error("Something went wrong.");
+  }
+}
 
 function App() {
-  return <div></div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function loadQuestion() {
+      try {
+        const response = await fetch("http://localhost:9000/questions");
+        const data = await response.json();
+
+        dispatch({ type: "dataFetched", payload: data });
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    loadQuestion();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header></Header>
+      <Main></Main>
+    </div>
+  );
 }
 
 export default App;
