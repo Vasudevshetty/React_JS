c182d67456b1ee5e84f1d5af82952a9b2cfefb6c
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-16 18:22:53 +0530

    Started react router v6+, where we can fetch as you render, define routing in a imperative way., Finsihed wih th baseic layout of the app, its time to configure well and style it usinmg tailwind.

---
+++ projects/fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return <div>
+      <Link to='/'>Fast React Pizza Co.</Link>
+      <p>vasu</p>
+  </div>;
+}
+
+export default Header;
