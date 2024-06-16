216414059a06337f9e50f934946f98c7fd964ded
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-16 18:53:14 +0530

    Started to use react router, loaded (fetched) as you render using loader property on the route options, and found a way to have the status of the fetch request(loading, idle) and displayed a loader, and even managed error using errorElement on the parent route ele(applayour), and even bubbled up to specific menu route.

--- projects/fast-react-pizza/src/App.jsx
+++ projects/fast-react-pizza/src/App.jsx
@@ -1,36 +1,42 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
-const router = createBrowserRouter([{
-  element: <AppLayout />,
-  children: [
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/menu",
-      element: <Menu />,
-    },
-    {
-      path: "/cart",
-      element: <Cart />,
-    },
-    {
-      path: "/order/new",
-      element: <CreateOrder />,
-    },
-    {
-      path: "/order/:orderId",
-      element: <Order />,
-    },
-  ],
-}]);
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
